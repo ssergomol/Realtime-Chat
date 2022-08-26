6e1bb882ace1a468a34d7f84a502c8673f7d9364
Sergey Molchanov <ser-mol2001@yandex.ru>
2022-08-27 00:52:57 +0300

    add messages table

--- backend/pkg/models/user.go
+++ backend/pkg/models/user.go
@@ -1,8 +1,19 @@
 package models
 
+import "gorm.io/gorm"
+
 type User struct {
-	Id       uint   `json:"id"`
-	Username string `json:"username"`
-	Email    string `json:"email" gorm:"unique"`
-	Password []byte `json:"-"`
+	gorm.Model
+	ID       uint      `json:"id"`
+	Username string    `json:"username"`
+	Email    string    `json:"email" gorm:"unique"`
+	Password []byte    `json:"-"`
+	Messages []Message `json:"messages"`
+}
+
+type Message struct {
+	gorm.Model
+	ID      uint   `json:"id"`
+	Content string `json:"content"`
+	UserID  uint   `json:"userid"`
 }
