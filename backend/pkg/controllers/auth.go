e13c882135e441a30b5073c7b962c4dab336bce8
Sergey Molchanov <ser-mol2001@yandex.ru>
2022-08-28 15:10:14 +0300

    fix preflight request handler

--- backend/pkg/controllers/auth.go
+++ backend/pkg/controllers/auth.go
@@ -17,72 +17,94 @@ import (
 const SecretKey = "secret"
 
 func SignUp(w http.ResponseWriter, r *http.Request) {
-	var data map[string]string
-	fmt.Printf("Got sign up request\n")
-	utils.ParseBody(r, &data)
-	fmt.Printf("Credentials: %s, %s, %s\n", data["username"], data["email"], data["password"])
+	switch r.Method {
+	case http.MethodPost:
+		var data map[string]string
+		fmt.Printf("Got sign up request\n")
+		utils.ParseBody(r, &data)
+		fmt.Printf("Credentials: %s, %s, %s\n", data["username"], data["email"], data["password"])
 
-	password, _ := bcrypt.GenerateFromPassword([]byte(data["password"]), 14)
+		password, _ := bcrypt.GenerateFromPassword([]byte(data["password"]), 14)
 
-	user := models.User{
-		Username: data["username"],
-		Email:    data["email"],
-		Password: password,
+		user := models.User{
+			Username: data["username"],
+			Email:    data["email"],
+			Password: password,
+		}
+		fmt.Printf("User object created!\n")
+		fmt.Println(database.DB.Create(&user))
+
+		res, _ := json.Marshal(user)
+		fmt.Print("res" + string(res) + "\n")
+		w.Header().Set("Content-Type", "application/json")
+		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+		w.Write(res)
+
+	case http.MethodOptions:
+		fmt.Printf("Got options req!\n")
+		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+		w.Header().Set("Access-Control-Allow-Methods", "*")
+		w.Header().Set("Access-Control-Allow-Headers", "*")
 	}
-	fmt.Printf("User object created!\n")
-	fmt.Println(database.DB.Create(&user))
 
-	res, _ := json.Marshal(user)
-	fmt.Print("res" + string(res) + "\n")
-	w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
-	w.Write(res)
 }
 
 func SignIn(w http.ResponseWriter, r *http.Request) {
-	var data map[string]string
+	switch r.Method {
+	case http.MethodPost:
+		var data map[string]string
 
-	utils.ParseBody(r, &data)
+		utils.ParseBody(r, &data)
 
-	var user models.User
+		var user models.User
 
-	database.DB.Where("username = ?", data["username"]).First(&user)
+		database.DB.Where("username = ?", data["username"]).First(&user)
 
-	if user.ID == 0 {
-		w.WriteHeader(http.StatusNotFound)
-		utils.SetBodyInfoMessage(w, "User not found")
-		return
+		if user.ID == 0 {
+			w.WriteHeader(http.StatusNotFound)
+			utils.SetBodyInfoMessage(w, "User not found")
+			return
+		}
+
+		if err := bcrypt.CompareHashAndPassword(user.Password, []byte(data["password"])); err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+
+			utils.SetBodyInfoMessage(w, "Incorrect password")
+			return
+		}
+
+		claims := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.StandardClaims{
+			Issuer:    strconv.Itoa(int(user.ID)),
+			ExpiresAt: time.Now().Add(time.Hour * 24).Unix(), //1 day
+		})
+
+		token, err := claims.SignedString([]byte(SecretKey))
+
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			utils.SetBodyInfoMessage(w, "Couldn't sign in")
+			return
+		}
+
+		cookie := &http.Cookie{
+			Name:     "jwt",
+			Value:    token,
+			Expires:  time.Now().Add(time.Hour * 24),
+			HttpOnly: true,
+		}
+
+		http.SetCookie(w, cookie)
+		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+		w.Header().Set("Access-Control-Allow-Credentials", "true")
+		utils.SetBodyInfoMessage(w, "Successfully signed in")
+
+	case http.MethodOptions:
+		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+		w.Header().Set("Access-Control-Allow-Methods", "*")
+		w.Header().Set("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization")
+		w.Header().Set("Access-Control-Allow-Credentials", "true")
+		w.Header().Set("Access-Control-Max-Age", "86400")
 	}
-
-	if err := bcrypt.CompareHashAndPassword(user.Password, []byte(data["password"])); err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-
-		utils.SetBodyInfoMessage(w, "Incorrect password")
-		return
-	}
-
-	claims := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.StandardClaims{
-		Issuer:    strconv.Itoa(int(user.ID)),
-		ExpiresAt: time.Now().Add(time.Hour * 24).Unix(), //1 day
-	})
-
-	token, err := claims.SignedString([]byte(SecretKey))
-
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		utils.SetBodyInfoMessage(w, "Couldn't sign in")
-		return
-	}
-
-	cookie := &http.Cookie{
-		Name:     "jwt",
-		Value:    token,
-		Expires:  time.Now().Add(time.Hour * 24),
-		HttpOnly: true,
-	}
-
-	http.SetCookie(w, cookie)
-	utils.SetBodyInfoMessage(w, "Successfully signed in")
-	return
 }
 
 func getUser(w http.ResponseWriter, r *http.Request) {
--- backend/pkg/routes/authRoutes.go
+++ backend/pkg/routes/authRoutes.go
@@ -1,20 +1,38 @@
 package routes
 
 import (
-	"net/http"
-
 	"github.com/gorilla/mux"
 	"github.com/ssergomol/RealtimeChat/pkg/controllers"
 )
 
 var RegisterAuthRoutes = func(router *mux.Router) {
-	router.HandleFunc("/sign-up", controllers.SignUp).Methods("POST")
-	router.HandleFunc("/sign-in", controllers.SignIn).Methods("POST")
-	router.HandleFunc("/sign-out", controllers.SignOut).Methods("POST")
-	router.Methods("OPTIONS").HandlerFunc(
-		func(w http.ResponseWriter, r *http.Request) {
-			w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
-			w.Header().Set("Access-Control-Allow-Methods", "*")
-			w.Header().Set("Access-Control-Allow-Headers", "*")
-		})
+	router.HandleFunc("/sign-up", controllers.SignUp).Methods("POST", "OPTIONS")
+	router.HandleFunc("/sign-in", controllers.SignIn).Methods("POST", "OPTIONS")
+	router.HandleFunc("/sign-out", controllers.SignOut).Methods("POST", "OPTIONS")
+
+	// router.HandleFunc("sign-up", func(w http.ResponseWriter, r *http.Request) {
+	// 	w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+	// 	w.Header().Set("Access-Control-Allow-Methods", "*")
+	// 	w.Header().Set("Access-Control-Allow-Headers", "*")
+	// }).Methods("OPTIONS")
+
+	// router.HandleFunc("sign-in", func(w http.ResponseWriter, r *http.Request) {
+	// 	w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+	// 	w.Header().Set("Access-Control-Allow-Methods", "*")
+	// 	w.Header().Set("Access-Control-Allow-Headers", "*")
+	// 	w.Header().Set("Access-Control-Allow-Credentials", "true")
+	// 	w.Header().Set("Access-Control-Max-Age", "86400")
+	// }).Methods("OPTIONS")
+
+	// router.Methods("OPTIONS").HandlerFunc(
+	// 	func(w http.ResponseWriter, r *http.Request) {
+	// 		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+	// 		w.Header().Set("Access-Control-Allow-Methods", "*")
+	// 		w.Header().Set("Access-Control-Allow-Headers", "*")
+	// 		w.Header().Set("Access-Control-Allow-Credentials", "true")
+	// 		w.Header().Set("Access-Control-Max-Age", "86400")
+	// 	})
+	// router.HandleFunc("sign-in", func(w http.ResponseWriter, r *http.Request) {
+
+	// }).Methods("OPTIONS")
 }
